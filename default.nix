1ffc4bf78ce0a2ddccfe33ba2a2e8af78ebde449
Anton Schirg <anton.schirg@posteo.de>
2024-02-18 12:53:50 -0500

    nix build definition for testing in larger project

--- default.nix
+++ default.nix
@@ -1,9 +1,15 @@
 let
   pkgs = import <nixpkgs> {};
   ninja = (import ../ninja);
-in pkgs.mkShell rec {
-  buildInputs = [
-    pkgs.llvmPackages.clang
-    ninja
-  ];
+in pkgs.rustPlatform.buildRustPackage rec {
+  pname = "ninjaview";
+  version = "0.0.1";
+
+  src = ./.;
+
+  postInstall = ''
+  cp ${ninja}/bin/ninja $out/bin
+  '';
+  
+  cargoSha256 = "sha256-/FtpSsRSvA6g9RbYtfRIUHoyAMRnGca4WN2FrmS14Hw=";
 }
